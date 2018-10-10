b74f0f13f165a569c50b776ea62819870ea52acb
dio-ub <hcgcarry@gmail.com>
2018-10-10 23:39:44 +0800

    fuck

---
+++ kerneltool/backup.sh
@@ -0,0 +1,2 @@
+sudo tar --exclude=/win --exclude=/sys --exclude=/proc --exclude=/media --exclude=/mnt --exclude=/tmp --exclude=/lost+found -zcvpf /media/dio/backup/linuxbackup/lastbackup.tgz /
+
---
+++ kerneltool/recovery.sh
@@ -0,0 +1,1 @@
+ tar xvpfz backup.tgz -C /
