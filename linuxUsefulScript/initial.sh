ac8a6e22e1c8e7cfac8d84a24c527c90b6260c39
dio-ubuntu <hcgcarry@gmail.com>
2020-02-15 20:59:54 +0800

    再刪

---
+++ linuxUsefulScript/initial.sh
@@ -0,0 +1,62 @@
+#install neovim:
+#if you try this command more than one time then it will throw error to get the key
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim 
+#i think it is because my internet width is occupy by youtube ,just shutdown youtube
+############################
+#install python-neovim:
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+#And then...
+
+pip install --user neovim
+
+pip3 install --user neovim
+####################################
+#install tmux:
+sudo apt-get install tmux
+#################################
+#install zsh:
+sudo apt-get install zsh
+
+#install oh-my-zsh:
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#change default shell to zsh
+chsh -s /bin/zsh 
+
+#install powerline:
+pip install powerline-status
+pip show powerline-status
+sudo apt-get install powerline
+
+#install autojump:
+sudo apt-get install autojump
+##############
+#install spacevim:
+curl -sLf https://spacevim.org/install.sh | bash
+#################
+#reference:https://www.digitalocean.com/community/tutorials/how-to-install-the-anaconda-python-distribution-on-ubuntu-16-04
+#install anaconda:這個看有沒需要再裝
+<<COMMENT
+
+cd /tmp
+curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+bash Anaconda3-5.0.1-Linux-x86_64.sh
+add export PATH="/path/to/dir:$PATH" to .zshrc
+COMMENT
+
+###############
+install gcin:
+https://magiclen.org/gcin/
+################
+install java8:
+sudo add-apt-repository ppa:webupd8team/java -y
+cd /var/lib/dpkg/info
+sudo sed -i 's|JAVA_VERSION=8u181|JAVA_VERSION=8u191|' oracle-java8-installer.*
+sudo sed -i 's|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/|' oracle-java8-installer.*
+sudo sed -i 's|SHA256SUM_TGZ="1845567095bfbfebd42ed0d09397939796d05456290fb20a83c476ba09f991d3"|SHA256SUM_TGZ="53c29507e2405a7ffdbba627e6d64856089b094867479edc5ede4105c1da0d65"|' oracle-java8-installer.*
+sudo sed -i 's|J_DIR=jdk1.8.0_181|J_DIR=jdk1.8.0_191|' oracle-java8-installer.*
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
